3ea9b582284fa1d9ac702f6134cdd0d3d8928bbd
Gabriel Garcia <gabrielproaj@gmail.com>
2025-06-12 12:47:01 -0300

    Department class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
 import db.DB;
+import model.entities.Department;
 
 import java.sql.Connection;
 
 
 public class Program {
     public static void main(String[] args) {
+
+        Department obj = new Department(1,"Books");
+        System.out.println(obj);
+
 
     }
 }
